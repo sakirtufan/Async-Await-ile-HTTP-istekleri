e74e9caa82a9c38637417a10b1cc033176b22f9c
sakir tufan <sakirtufan49@gmail.com>
2020-07-27 18:23:14 +0200

    modified app.js

--- app.js
+++ app.js
@@ -1,83 +1,123 @@
 class Request {
-   async get(url){
-      
-      const response =await fetch(url); //response object aldik
+   async get(url) { //get request
 
-      const data1 = await response.json();  //JSON object
+      // return new Promise((resolve, reject) => {
 
-      return data1;  //promise objesi seklinde döndürdük
+      //    fetch(url)
+      //       .then(response => response.json())
+      //       .then(data => resolve(data))
+      //       .catch(err => reject(err));
+
+      // })
+
+      const response = await fetch(url);  //response objeyi aldik
+
+      const data = await response.json();  //response objesini jsona cevirdik
+
+      return data;  //disarda kullanmak icin döndürdük
+
    }
 
+   async post(url, data) {
 
-   async post(url,data){
+      // return new Promise((resolve, reject) => {
+      //    fetch(url, {
+      //          method: 'POST',
+      //          body: JSON.stringify(data),
+      //          headers: {
+      //             "Content-type": "application/json; charset=UTF-8"
+      //          }
+      //       })
+      //       .then(response => response.json())
+      //       .then(data => resolve(data))
+      //       .catch(err => reject(err));
+      // })
 
       const response = await fetch(url,{
-         method:'POST',
-         body:JSON.stringify(data),
-         headers:{
-            "Content-type" : "application/json; charset=UTF-8"
-         }
-      });  //response objesi gelir
+                  method: 'POST',
+                  body: JSON.stringify(data),
+                  headers: {
+                     "Content-type": "application/json; charset=UTF-8"
+                  }
+               });
 
-      const data2 = await response.json();
+      const responsedata = await response.json();
 
-      return data2;
+      return responsedata;
    }
 
 
-   async put(url,data){
+   async put(url, data) {
+
+      // return new Promise((resolve,reject)=>{
+      //    fetch(url, {
+      //       method: 'PUT',
+      //       body: JSON.stringify(data),
+      //       headers: {
+      //          "Content-type": "application/json; charset=UTF-8"
+      //       }
+      //    })
+      //    .then(response => response.json())
+      //    .then(data => resolve(data))
+      //    .catch(err => reject(err));
+      // })
 
       const response = await fetch(url,{
-         method:'PUT',
-         body:JSON.stringify(data),
-         headers:{
-            "Content-type" : "application/json; charset=UTF-8"
-         }
-      });  //response OBJECT
+               method: 'PUT',
+               body: JSON.stringify(data),
+               headers: {
+                  "Content-type": "application/json; charset=UTF-8"
+               }
+            });
 
-      const data3 = response.json();
+      const responsedata = await response.json();
 
-      return data3;
+      return responsedata;
+
    }
-
 
    async delete(url){
-      
+      // return new Promise((resolve,reject) =>{
+      //    fetch(url,{
+      //       method:'DELETE'
+      //    }).then(response => resolve('silinme islemi basarili'))
+      //    .catch(err => reject(err));
+      // })
+
       const response = await fetch(url,{
-         method:'DELETE'
-      });
-
-      // const data = response.json();
-
+               method:'DELETE'
+            });
+      
+      
       return 'veri silme islemi basarili';
    }
+
+
+
+
 }
 
 const request = new Request();
 
-request.get('https://jsonplaceholder.typicode.com/albums').then((result) => {
-   console.log(result);
-}).catch((err) => {
-   error.log(err);
-});
+
+
+// request.get("https://jsonplaceholder.typicode.com/albums")
+//    .then(albums => {
+//       console.log(albums);
+//    })
+//    .catch(err => console.log(err));
 
 
 
-request.post('https://jsonplaceholder.typicode.com/posts',{"userId":1,"title":'thriller'}).then((result) => {console.log(result);
-   
-}).catch((err) => {console.log(err);
-   
-});
-
-request.put('https://jsonplaceholder.typicode.com/posts/1',{"userId":1,"title":'sakir'}).then((result) => {console.log(result);
-   
-}).catch((err) => {error.log(err);
-   
-});
+// request.post("https://jsonplaceholder.typicode.com/albums",{userId:10,title:'merhaba'})
+// .then(newAlbum =>console.log(newAlbum))
+// .catch(err =>console.log(err));
 
 
-request.delete('https://jsonplaceholder.typicode.com/posts/1').then((result) => {
-   console.log(result);
-}).catch((err) => {
-   error.log(err);
-});
+// request.put("https://jsonplaceholder.typicode.com/albums/1",{userId:10,title:'tarkan'})
+// .then(album => console.log(album))
+// .catch(err => console.log(err));
+
+// request.delete("https://jsonplaceholder.typicode.com/albums/1")
+// .then(message => console.log(message))
+// .catch(err => console.log(err));
